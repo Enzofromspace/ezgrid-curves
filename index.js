600de75d29c095688f03c8576866bf7eee7b3b70
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-02 13:30:21 +0300

    added points and grid - commit before for loop test (it breaks stackblitz when I get it wrong lol)

--- index.js
+++ index.js
@@ -5,6 +5,32 @@ const settings = {
 };
 
 const sketch = () => {
+  const cols = 12;
+  const rows = 6;
+  const numCells = cols * rows;
+  //grid begins
+  const gw = width * 0.8;
+  const gh = height * 0.8;
+  //cell
+  const cw = gw / cols;
+  const ch = gh / rows;
+  //margin
+  const mx = (width - gw) * 0.5;
+  const my = (height - gh) * 0.5; 
+  //an array to store the points we create in for loop
+  const points = [];
+
+  let x, y;
+
+  /*
+  for (let i = 0; i < numCells; i++){
+    x = (i % cols) * cw;
+    y = Math.Floor(i / cols) * ch;
+
+    points.push(new Point({x,y}));
+  }
+  */
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
@@ -12,3 +38,22 @@ const sketch = () => {
 };
 
 canvasSketch(sketch, settings);
+
+class Point {
+  constructor({x,y}){
+    this.x = x;
+    this.y = y;
+  }
+
+  draw(context){
+    context.save();
+    context.translate(this.x, this.y);
+    context.fillStyle = `darkorange`;
+
+    context.beginPath();
+    context.arc(0,0,10,0 Math.PI *2);
+    context.fill();
+
+    context.restore();
+  }
+}
