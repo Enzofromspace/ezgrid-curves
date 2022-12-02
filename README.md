9a7c7b326ce161bd5777198a9b633120e7d7d979
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-02 12:02:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ezgrid-curves
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-s7nqxb)
---
+++ index.js
@@ -0,0 +1,14 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [1080, 1080],
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+  };
+};
+
+canvasSketch(sketch, settings);
